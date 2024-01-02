e82408f96dd3be5b7cbe7223484ce17764ceed7e
Navneet Thakur <thakurnavneet686@gmail.com>
2024-01-03 00:28:13 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<body>
+<?php require_once "header.php"?>
+    <h1>this is index page!</h1>
+    <button><a href="login.php">Login</a></button>
+</body>
---
+++ news_.sql
@@ -0,0 +1,9 @@
+-- creating main user id and password  
+-- O23MCA110202
+
+CREATE TABLE Admin_users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
